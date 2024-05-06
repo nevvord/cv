8ee350b504e6f8c66975cff43da8f392bb0bb4df
Nevvord <nevvord@gmail.com>
2024-05-06 18:56:15 +0200

    try update base cv

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import svgr from 'vite-plugin-svgr'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svgr(), react()],
-  base: '/cv',
+  // base: '/cv',
   optimizeDeps: {
     include: ['@mui/material'],
   },
